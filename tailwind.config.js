4b23293f956434e8d77c19304f7a5c7fbe042810
ĐỒNG MINH PHÚ <minhphu1509@gmail.com>
2026-01-04 22:51:23 +0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
     content: [
         "./index.html",
-        "./**/*.{js,ts,jsx,tsx}",
+        "./index.tsx",
+        "./App.tsx",
+        "./AppContext.tsx",
+        "./types.ts",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./services/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
         extend: {
