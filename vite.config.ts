3c9dfc3889996208e9aecee7e2a73482fc468c1c
ĐỒNG MINH PHÚ <minhphu1509@gmail.com>
2026-01-04 22:35:30 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      envPrefix: ['VITE_', 'GEMINI_'],
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  return {
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [react()],
+    envPrefix: ['VITE_', 'GEMINI_'],
+    resolve: {
+      extensions: ['.tsx', '.ts', '.jsx', '.js'],
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
+
